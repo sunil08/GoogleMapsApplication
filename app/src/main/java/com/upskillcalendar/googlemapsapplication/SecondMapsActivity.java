64deb37dab69a1469284bd9470142cba644297be
Sunil Kumar <sunilcode90@gmail.com>
2016-08-09 18:16:01 +0530

    This is the third commit of Google Maps.

--- app/src/main/java/com/upskillcalendar/googlemapsapplication/SecondMapsActivity.java
+++ app/src/main/java/com/upskillcalendar/googlemapsapplication/SecondMapsActivity.java
@@ -1,14 +1,29 @@
 package com.upskillcalendar.googlemapsapplication;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class SecondMapsActivity extends AppCompatActivity implements OnMapReadyCallback{
@@ -33,8 +48,42 @@ public class SecondMapsActivity extends AppCompatActivity implements OnMapReadyC
 
         // Add a marker in Delhi and move the camera
         LatLng markerLatLong = new LatLng(28.6139, 77.2090);
-       /* mMap.addMarker(new MarkerOptions()
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location))
-                .position(markerLatLong));*/
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location));
+
+        markerOptions.position(markerLatLong).title("Delhi").draggable(true);
+        map.addMarker(markerOptions);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(28.6139, 77.2090),10.0f));
+
+
+        // setOnCameraChangeListener and setOnMarkerDragListener does not work simultaneously and at the same time so embed camera change in drag listener
+
+/*        map.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(28.6139, 77.2090),map.getCameraPosition().zoom));
+            }
+        });*/
+
+        map.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                Log.e("Lat Long ", "latitude : "+ marker.getPosition().latitude + " longitude : "+ marker.getPosition().longitude);
+                marker.setSnippet(Double.toString(marker.getPosition().latitude));
+                map.animateCamera(CameraUpdateFactory.newLatLng(marker.getPosition()));
+            }
+        });
     }
+
 }
